9e25367d9cd2b6eeef6dfb4e4925649a58bec37f
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-10-22 15:21:55 +0530

    migration change

--- Backend/prisma/migrations/20241022045140_wellbee1_1/migration.sql
+++ Backend/prisma/migrations/20241022045140_wellbee1_1/migration.sql
@@ -1,13 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'THERAPIST');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "profilePic" TEXT,
     "height" DOUBLE PRECISION,
     "weight" DOUBLE PRECISION,
-    "mood" TEXT,
-    "role" TEXT NOT NULL DEFAULT 'user',
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -15,22 +19,11 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "CoachingTips" (
-    "id" TEXT NOT NULL,
-    "height" DOUBLE PRECISION NOT NULL,
-    "weight" DOUBLE PRECISION NOT NULL,
-    "bmi" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL,
-
-    CONSTRAINT "CoachingTips_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "ExerciseTip" (
     "id" TEXT NOT NULL,
     "tip" TEXT NOT NULL,
-    "coachingTipsId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "ExerciseTip_pkey" PRIMARY KEY ("id")
 );
@@ -39,7 +32,8 @@ CREATE TABLE "ExerciseTip" (
 CREATE TABLE "DietTip" (
     "id" TEXT NOT NULL,
     "tip" TEXT NOT NULL,
-    "coachingTipsId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "DietTip_pkey" PRIMARY KEY ("id")
 );
@@ -56,23 +50,13 @@ CREATE TABLE "ChatMessage" (
 );
 
 -- CreateTable
-CREATE TABLE "Mood" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "moodState" TEXT NOT NULL,
-    "musicRecommendation" TEXT,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Mood_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "TherapyAppointment" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "therapistId" TEXT NOT NULL,
     "appointmentDate" TIMESTAMP(3) NOT NULL,
     "status" TEXT NOT NULL DEFAULT 'pending',
+    "googleMeet" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "TherapyAppointment_pkey" PRIMARY KEY ("id")
@@ -98,23 +82,6 @@ CREATE TABLE "AvailableSlot" (
 );
 
 -- CreateTable
-CREATE TABLE "Music" (
-    "id" TEXT NOT NULL,
-    "mood" TEXT NOT NULL,
-
-    CONSTRAINT "Music_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Track" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "musicId" TEXT NOT NULL,
-
-    CONSTRAINT "Track_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "HealthMetrics" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -134,19 +101,13 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "TherapistProfile_userId_key" ON "TherapistProfile"("userId");
 
 -- AddForeignKey
-ALTER TABLE "CoachingTips" ADD CONSTRAINT "CoachingTips_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ExerciseTip" ADD CONSTRAINT "ExerciseTip_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ExerciseTip" ADD CONSTRAINT "ExerciseTip_coachingTipsId_fkey" FOREIGN KEY ("coachingTipsId") REFERENCES "CoachingTips"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "DietTip" ADD CONSTRAINT "DietTip_coachingTipsId_fkey" FOREIGN KEY ("coachingTipsId") REFERENCES "CoachingTips"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "DietTip" ADD CONSTRAINT "DietTip_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ChatMessage" ADD CONSTRAINT "ChatMessage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Mood" ADD CONSTRAINT "Mood_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "TherapyAppointment" ADD CONSTRAINT "TherapyAppointment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -159,9 +120,6 @@ ALTER TABLE "TherapistProfile" ADD CONSTRAINT "TherapistProfile_userId_fkey" FOR
 
 -- AddForeignKey
 ALTER TABLE "AvailableSlot" ADD CONSTRAINT "AvailableSlot_therapistProfileId_fkey" FOREIGN KEY ("therapistProfileId") REFERENCES "TherapistProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Track" ADD CONSTRAINT "Track_musicId_fkey" FOREIGN KEY ("musicId") REFERENCES "Music"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "HealthMetrics" ADD CONSTRAINT "HealthMetrics_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
